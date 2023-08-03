4a14be2c44ab28c7c6d0ac2da9bb495a374a4451
JoshRees-Haughton <joshreeshaughton@proton.megit config --global user.name JoshRees-Haughtongit config --global user.email joshreeshaughton@proton.me>
2023-08-03 16:55:28 +0100

    feat: basic functionality added for getting roman numeral from digit

--- roman_numeral_converter.js
+++ roman_numeral_converter.js
@@ -3,61 +3,39 @@ function convertToRoman(num) {
     let strNum = num.toString();
   
     //Get length of num string
-    let strLength = strNum.length
+    let strLength = strNum.length;
     //console.log(strLength)
   
     //Define empty output string
-    let strOut = ''
+    let strOut = '';
   
-    //Define map to link to other maps:
+    //Define 2d array for roman numerals:
+    let arabicToRomanArr = [['I', 'IV', 'V', 'IX'], ['X', 'XL', 'L', 'XC'], ['C', 'CD', 'D', 'CM'], ['M']];
   
-    /*Define maps of Arabic values and Roman numerals for each power of ten:
-    const arabicToRomanThousand = new Map([
-    ["1000", 'M']
-  ]);
-    const arabicToRomanHundred = new Map([
-    ["900", 'CM'],
-    ["500", 'D'],
-    ["400", 'CD'],
-    ["100", 'C']
-  ]);
-    const arabicToRomanTen = new Map([
-    ["90", 'XC'],
-    ["50", 'L'],
-    ["40", 'XL'],
-    ["10", 'X']
-  ]);
-    const arabicToRomaOne = new Map([
-    ["9", 'IX'],
-    ["5", 'V'],
-    ["4", 'IV'],
-    ["1", 'I']
-  ]);
-  */
   
   
     //Loop through the num string
     for (let i = 0; i < strLength; i++) {
   
-      //Convert digit into roman char based on index
-      let digitDec = strNum[i] * (10 ** (strLength - i - 1));
-      console.log(digitDec)
+      //Convert digit into roman char based on index:
+      //Index of the coverting array to use in each loop:
+      let convertArrIndex = strLength - i - 1;
   
-      //Break down into individual units:
+      //Character converted to a string:
+      let digitStr = strNum[i]
+      console.log(digitStr)
   
-      //Converts from the decimal to the string:
-      let digitDecString = digitDec.toString()
-      console.log(digitDecString)
+      //Get Roman character from array for basic case:
+      let romanCharOne = arabicToRomanArr[convertArrIndex][0]
+      console.log(romanCharOne)
+      
   
-      //Gets the value from the map:
-      let romanChar = arabicToRoman.get(digitDecString)
-      console.log(romanChar)
       //Add to out string
       
     }
   
   
-    console.log(strNum)
+    //console.log(strNum)
    return strOut;
   }
   
